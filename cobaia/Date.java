3d71e2e4fcc178eb85581037a46245d3c6b1b36a
Marcio Ramos <marciojrtorres@gmail.com>
2014-08-26 20:13:22 -0300

    util atualizado

--- cobaia/Date.java
+++ cobaia/Date.java
@@ -1,9 +1,21 @@
 public class Date {
+
     public static String toISO(String s) {
         return s.split("/")[2] + "-" + s.split("/")[1] + "-" + s.split("/")[0];
     }
 
-    public static void main(String[] args) {
-        System.out.println(Date.toISO("13/08/2014"));
+    public static String formatAsISO(String s) {
+        String[] partes = s.split("/");
+
+        String ano = partes[2];
+        String mes = partes[1];
+        String dia = partes[0];
+
+        return ano + "-" + mes + "-" + dia;
     }
+
+    public static void main(String[] args) {
+        System.out.println(Date.dias(2, 2012));
+    }
+
 }
